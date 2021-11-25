a246db649b798b1d51c80e8d4ac69c05f2bdc733
Alex Eagleson <alexeagleson@gmail.com>
2021-11-25 10:58:13 -0500

    update storybook to webpack

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,6 +5,10 @@ module.exports = {
   ],
   "addons": [
     "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
+    "@storybook/addon-essentials",
+    "@storybook/preset-scss"
+  ],
+  "core": {
+    "builder": "webpack5"
+  }
 }
